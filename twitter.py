45fa6223277ec179e655f5469e505f87d2bc4ef6
Breyanna Vass <breyannavass@gmail.com>
2020-01-29 13:01:20 -0500

    adding in the ability to show followers

--- twitter.py
+++ twitter.py
@@ -2,7 +2,26 @@ import tweepy
 import time
 
 # include the API Key
-auth = tweepy.OAuthHandler('', '')
+auth = tweepy.OAuthHandler(‘APIKEY’, ‘APIKEYSECRET’)
 
 # access token and access token
-auth.set_access_token('', '')
+auth.set_access_token(‘SAT’, ‘SATP’)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+# show user
+# print(user)
+
+# show user screen name
+# print(user.screen_name)
+
+# Shows the followers for this twitter account using cursor
+for follower in tweepy.Cursor(api.followers).items():
+    print(follower.name)
+
+# follow back a follower
+# for follower in tweepy.Cursor(api.followers).item():
+#     if follower.name == '':
+#         follower.follow()
