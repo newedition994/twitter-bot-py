05fa5592a77ccdd31d263f898487ebfbed401588
Breyanna Vass <breyannavass@gmail.com>
2020-01-29 12:44:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Twitter Bot
+
+Creating a twitter bot using python to do auto likes and retweets.
---
+++ twitter.py
@@ -0,0 +1,8 @@
+import tweepy
+import time
+
+# include the API Key
+auth = tweepy.OAuthHandler('', '')
+
+# access token and access token
+auth.set_access_token('', '')
