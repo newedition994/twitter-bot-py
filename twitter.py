6dc43a7669b45a2bda7d8a32df37599d3f91606b
Breyanna Vass <breyannavass@gmail.com>
2020-01-29 13:14:27 -0500

    added the ability to like tweets and retweet them as well

--- twitter.py
+++ twitter.py
@@ -18,10 +18,35 @@ user = api.me()
 # print(user.screen_name)
 
 # Shows the followers for this twitter account using cursor
-for follower in tweepy.Cursor(api.followers).items():
-    print(follower.name)
+# for follower in tweepy.Cursor(api.followers).items():
+#     print(follower.name)
 
 # follow back a follower
 # for follower in tweepy.Cursor(api.followers).item():
 #     if follower.name == '':
 #         follower.follow()
+
+# gather search terms and number
+search = 'Javascript'
+nrTweets = 50
+
+for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+    try:
+        print('Tweet liked')
+        tweet.favorite()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+# to retweet a tweet
+# for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+#     try:
+#         print('Retweeted')
+#         tweet.retweet()
+#         time.sleep(10)
+#     except tweepy.TweepError as e:
+#         print(e.reason)
+#     except StopIteration:
+#         break
